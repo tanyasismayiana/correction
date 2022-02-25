b9d7e21b9699a23ec9326d0b311729f137a580f9
tanyamayiana <tanyamayiana@gmail.com>
2022-02-25 12:14:21 +0300

    correction

---
+++ src/func.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var username: String = ("tanya")
+    var age= 21
+    println("hello ,$username")
+    println (age)
+    var y = moduluse(45 ,6)
+    println(y)
+    var total = add(4 , 6 , 7, 5)
+    println(total)
+    println("swimming")
+}
+fun sayHello(){
+    println("hello")
+}
+fun moduluse(num1:Int , num2:Int):Int{
+    var divition = (num1 % num2)
+    return divition
+}
+fun add(num1:Int ,num2:Int ,num3:Int ,num4:Int):Int{
+    var sum =(num1 +num2 + num3 + num4)
+    return sum
+}
+fun funFact() {
+    println("swimming")
+}
